31bc8bee3179fd789003174158ec6f6678cf3a1c
Stefan Ravn van Overeem <stefan@stefanrvo.dk>
2015-10-07 02:19:19 +0200

    Added max output (Workshop 2)

--- Workshop 2/Code/main.py
+++ Workshop 2/Code/main.py
@@ -8,7 +8,7 @@ import numpy as np
 def __main__():
 	P = SimplePendulum(g = -9.82, b = 1, m = 1, l = 1, timestep = .01, \
 		init_angle = radians(5), init_angle_speed = 0)
-	C = SimplePendulumController(P, outmax = None, outmin = None)
+	C = SimplePendulumController(P, outmax = 100, outmin = -100)
 	Plotter = DrawPendulum(P, C)
 	Plotter.startAnimation()
 	
